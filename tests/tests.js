9364c7b3ed11c7dc2a08277b24e73d4c9c0730ca
sttheis2 <sttheis2@illinois.edu>
2018-12-10 22:19:44 -0600

    fixed a test...

--- tests/tests.js
+++ tests/tests.js
@@ -7,10 +7,15 @@ function runTests() {
 function testMoveLayer() {
   testLayerManager.reset();
   testLayerManager.addLayer();
-  testLayerManager.moveLayer(0, 1);
 
   var layer0 = testLayerManager.getLayer(0);
-  if(layer0.canvas.style.zIndex == 1) {
+  var layer1 = testLayerManager.getLayer(1);
+
+  var oldLayer1zIndex = layer1.canvas.style.zIndex;
+
+  testLayerManager.moveLayer(0, 1);
+
+  if(layer0.canvas.style.zIndex == oldLayer1zIndex) {
     console.log("Test PASSED: testMoveLayer");
   } else {
     console.log("Test FAILED: testMoveLayer");
